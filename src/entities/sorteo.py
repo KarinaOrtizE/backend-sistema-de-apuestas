bac7ce8bd4b7fc748969143eff1cd49b72a51106
JuaMorales25 <moralescartagenajuanmanuel3@gmail.com>
2026-03-18 21:13:39 -0500

    feat: agregar claves foraneas y relaciones a sorteo

--- src/entities/sorteo.py
+++ src/entities/sorteo.py
@@ -1,29 +1,30 @@
 import uuid
-from sqlalchemy import Column, DateTime, func
+from sqlalchemy import Column, DateTime, ForeignKey, func
 from sqlalchemy.dialects.postgresql import UUID, JSONB
 from sqlalchemy.orm import relationship
 from src.database.config import Base
 
 
 class Sorteo(Base):
-
     __tablename__ = "sorteo"
 
     id_sorteo = Column(
         UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, index=True
     )
-
     fecha_sorteo = Column(DateTime(timezone=True), nullable=False)
-
     resultado = Column(JSONB, nullable=True)
+    fecha_creacion = Column(DateTime(timezone=True), server_default=func.now())
 
-    fecha_creacion = Column(
-        DateTime(timezone=True),
-        server_default=func.now(),
-        description="Fecha en que se programó el sorteo",
+    id_bingo = Column(UUID(as_uuid=True), ForeignKey("bingo.id_bingo"), nullable=True)
+    id_ruleta = Column(
+        UUID(as_uuid=True), ForeignKey("ruleta.id_ruleta"), nullable=True
+    )
+    id_loteria = Column(
+        UUID(as_uuid=True), ForeignKey("loteria.id_loteria"), nullable=True
     )
 
-    apuestas = relationship("Apuesta", back_populates="sorteo")
+    bingo = relationship("Bingo", back_populates="sorteos")
+    ruleta = relationship("Ruleta", back_populates="sorteos")
+    loteria = relationship("Loteria", back_populates="sorteos")
 
-    def __repr__(self):
-        return f"<Sorteo(id='{self.id_sorteo}', fecha='{self.fecha_sorteo}')>"
+    apuestas = relationship("Apuesta", back_populates="sorteo")
